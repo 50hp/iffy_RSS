9fee5fe391d96b16ca0fbb777859e53cc6f3ced2
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-26 16:09:08 -0500

    Create the server side route for geting user feeds

--- server/routes/rss_source.js
+++ server/routes/rss_source.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const { Prompt } = require('react-router-dom/cjs/react-router-dom.min');
 const pool = require('../modules/pool');
 const router = express.Router();
 // const rssSource =  require('../modules/rss.js');
 
-// console.log(rssSource);
 
-
-
-router.get('/:id', (req, res) => {
+router.get('/', (req, res) => {
+    
+    if (req.isAuthenticated()) {
+        const idToGet = req.user.id; 
+        const queryText = `SELECT * FROM feeds
+                           JOIN rss_sources ON feeds.rss_id = rss_sources.id
+                           WHERE user_id = $1;`;
+        pool.query(queryText, [idToGet])
+        .then(results => {
+            res.send(results.rows);
+        }).catch(error => {
+            console.log('error with query', queryText, error);
+            res.sendStatus(500);
+        });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 /**
