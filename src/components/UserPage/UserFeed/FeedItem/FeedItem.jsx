b6094a14994f3901cfa245f9b50c92477ddba8ae
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-27 09:55:47 -0500

    created a save and read button that are conditionaly render based on local state. created a function that switches the loacl state of the buttons when called. created two dispatch calls for the buttons

--- src/components/UserPage/UserFeed/FeedItem/FeedItem.jsx
+++ src/components/UserPage/UserFeed/FeedItem/FeedItem.jsx
@@ -1,12 +1,46 @@
-
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 function FeedItem({item}) {
+    
+    const dispatch = useDispatch();
+    const [markRead, setMarkRead] = useState(false);
+    const [markSave, setMarkSave] = useState(false);
+    const handleClick = (button) => {
+        switch(button) {
+        case 'read':
+            setMarkRead(!markRead);
+            dispatch({type:"SET_READ", payload:!markRead});
+            return;
+        case 'save':
+            setMarkSave(!markSave);
+            dispatch({type:"SET_SAVE", payload:!markSave});
+            return; 
+        }
+    }
+
+
+
 
     return(
         <div>
+
             <h4>{item.title}</h4>
             <span>{item.creator}</span>
             <p dangerouslySetInnerHTML={{__html:item.content}}/>
+            
+            {markRead ? (
+                <button onClick={()=> handleClick('read')}> Mark as UnRead </button>
+                ) : ( 
+                <button onClick={()=> handleClick('read')}> Mark as Read </button>
+                )}
+            {markSave ? (
+                <button onClick={()=> handleClick('save')}> UnSave </button>
+                ) : (
+                <button onClick={()=> handleClick('save')}> Save </button>
+                )} 
+
+
         </div>
     );
 
