eb3d645ccc7b08ceb54afaf39858f49696358e6d
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-27 10:38:52 -0500

    created a saga for updating read and save

--- src/redux/sagas/feed.saga.js
+++ src/redux/sagas/feed.saga.js
@@ -1,4 +1,4 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, take, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 
@@ -14,9 +14,32 @@ function* fetchFeed() {
     }
 }
 
+function* setRead(action) {
+    console.log('insetread', action)
+    try {
+        yield axios.put(`/api/rss/read/${action.payload.id}`, action.payload.state)
+        console.log('success');
+    }
+    catch {
+        console.log('error with updating read');
+    }
+} 
+
+function* setSave(action) {
+    console.log('insetsave', action);
+    try {
+        yield axios.put(`/api/rss/save/${action.payload.id}`, action.payload.state);
+        console.log('success');
+    }
+    catch {
+        console.log('error with updating save');
+    }
+}
 
 function* feedSaga() {
     yield takeLatest("FETCH_FEED", fetchFeed);
+    yield takeLatest("SET_READ", setRead);
+    yield takeLatest("SET_SAVE", setSave);
 
 }
 
