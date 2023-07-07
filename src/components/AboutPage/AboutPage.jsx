45ce16dcceecf4e44bf008f3657181f243fdf225
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-07-07 13:25:49 -0500

    started putting information on about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -5,7 +5,51 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p></p>
+              <p>
+
+    For getting the rss feed from any subreddit just put .rss to the end of the url
+
+    Reddit 
+       
+       Aliens
+       https://www.reddit.com/r/aliens/.rss
+
+       Memes
+       https://www.reddit.com/r/memes/.rss
+
+       technicallythetruth
+       https://www.reddit.com/r/technicallythetruth/.rss
+
+       Showerthoughts
+       https://www.reddit.com/r/Showerthoughts/.rss
+
+       gaming
+       https://www.reddit.com/r/gaming/.rss
+
+       antiwork
+       https://www.reddit.com/r/antiwork/.rss
+
+       worldnews
+       https://www.reddit.com/r/worldnews/.rss
+
+       ProgrammerHumor
+       https://www.reddit.com/r/ProgrammerHumor/.rss
+
+       YouShouldKnow
+       https://www.reddit.com/r/YouShouldKnow/.rss
+
+
+    Reddit Post
+
+        Aliens?
+        https://www.reddit.com/r/aliens/comments/14rp7w9/from_the_late_2000s_to_the_mid2010s_i_worked_as_a/.rss
+
+
+
+
+
+
+        </p>
       </div>
     </div>
   );
